81cc6a7a17d8157b9ae34d2fdf23c459da956009
RoundBridge <410308507@qq.com>
2020-09-26 20:11:27 +0800

    optimize the read frame operation and support decode

--- XPlay1/XFFmpeg.cpp
+++ XPlay1/XFFmpeg.cpp
@@ -26,29 +26,67 @@ int XFFmpeg::open(const char* path) {
 	// 打开成功的情况下，把关于这个文件的相关信息都更新好，便于外部使用
 	if (0 == re) {
 		compute_duration_ms();
+		re = create_decoder(ic);
 	}
 	mutex.unlock();
 
 	return re;
 }
 
-AVPacket XFFmpeg::read() {
-	AVPacket pkt;
+AVPacket* XFFmpeg::read() {
 	int re = 0;
-
+	// read函数里面不负责对packet的释放操作，需要外部释放
+	if (packet == NULL) {
+		packet = av_packet_alloc();
+	}
 	mutex.lock();
-	memset((void *)&pkt, 0, sizeof(AVPacket));
 	if (!ic) {
 		mutex.unlock();
-		cout << "Please open first!" << endl;
-		return pkt;
+		cout << "[PACKET] Please open first!" << endl;
+		return NULL;
 	}	
-	re = av_read_frame(ic, &pkt);
+	re = av_read_frame(ic, packet);
 	if (0 != re) {
-		cout << "read frame error: " << get_error(re) << endl;
+		mutex.unlock();
+		cout << "[PACKET] read frame error: " << get_error(re) << endl;
+		return NULL;
 	}
 	mutex.unlock();
-	return pkt;
+	return packet;
+}
+
+AVFrame* XFFmpeg::decode(const AVPacket* pkt) {
+	AVCodecContext* cc = NULL;
+
+	if (yuv == NULL) {
+		yuv = av_frame_alloc();
+	}
+
+	if (pkt->stream_index == videoStream) { cc = vc; }
+	if (pkt->stream_index == audioStream) { cc = ac; }
+
+	mutex.lock();
+	// 如果是视频包，pkt中的stream_index直接指明视频流的idx，音频包同理
+	// 发送packet到解码线程，只是将pkt扔给解码线程，几乎不占用CPU
+	// send NULL后调用多次receive可以取出所有缓冲帧
+	int re = avcodec_send_packet(cc, pkt);
+	if (0 != re) {
+		mutex.unlock();
+		cout << "[DECODE] avcodec_send_packet failed: " << get_error(re) << endl;
+		return NULL;
+	}
+	// 只是从解码线程中获取解码结果，几乎不占用CPU，一次send（一次可能发送多个音频帧）可能对应多次receive
+	// 为什么这里只获取一次？难道不应该多获取几次，把里面的帧全部获取出来？
+	// 下面avcodec_receive_frame的第一个参数不能用ic->streams[pkt->stream_index]->codec了，因为它已经是
+	// 废弃属性了，用它的话，函数调用失败
+	re = avcodec_receive_frame(cc, yuv);
+	if (0 != re) {
+		mutex.unlock();
+		cout << "[DECODE] avcodec_receive_frame failed: " << get_error(re) << endl;
+		return NULL;
+	}
+	mutex.unlock();
+	return yuv;
 }
 
 void XFFmpeg::compute_duration_ms() {
@@ -60,14 +98,108 @@ void XFFmpeg::compute_duration_ms() {
 	}
 	return;
 }
+
+bool XFFmpeg::create_decoder(AVFormatContext* ic) {
+	int re = 0;
+
+	if (!ic) {
+		cout << "[CREATE DECODER] Please open first!" << endl;
+		return -5;
+	}
+
+	//函数获取videoStream和audioStream，然后代入到ic->streams[i]中获取各个流的信息，就不用遍历了
+	videoStream = av_find_best_stream(ic, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0);  // 倒数第二个参数传NULL是因为封装和解码隔离开不耦合？
+	audioStream = av_find_best_stream(ic, AVMEDIA_TYPE_AUDIO, -1, -1, NULL, 0);
+	cout << "[CREATE DECODER] videoStream = " << videoStream << endl;
+	cout << "[CREATE DECODER] audioStream = " << audioStream << endl;
+
+	/************************************ 视频解码器部分 ************************************/
+	//找到视频解码器
+	AVCodec* vcodec = avcodec_find_decoder(ic->streams[videoStream]->codecpar->codec_id);
+	if (!vcodec) {
+		cout << "[CREATE DECODER] Can't find the video codec id " << ic->streams[videoStream]->codecpar->codec_id << endl;
+		return -1;
+	}
+	else {
+		cout << "[CREATE DECODER] Find the vcodec " << ic->streams[videoStream]->codecpar->codec_id << endl;
+	}
+
+	//创建解码器上下文
+	if (vc == NULL) { vc = avcodec_alloc_context3(vcodec); }	
+
+	//配置解码器上下文参数
+	//新版本已经将AVStream结构体中的AVCodecContext字段定义为废弃属性。
+	//因此无法像旧的版本直接通过AVFormatContext获取到AVCodecContext结构体参数
+	//当前版本保存视音频流信息的结构体AVCodecParameters，FFmpeg提供了函数avcodec_parameters_to_context
+	//将音视频流信息拷贝到新的AVCodecContext结构体中
+	avcodec_parameters_to_context(vc, ic->streams[videoStream]->codecpar);
+
+	//4线程解码
+	//也可以先通过linux或者Windows API来获取CPU个数进而决定解码线程数
+	vc->thread_count = 4;
+
+	//打开解码器上下文
+	re = avcodec_open2(vc, 0, 0);
+	if (0 != re) {
+		cout << "[CREATE DECODER] avcodec_open2 failed: " << get_error(re) << endl;
+		return -2;
+	}
+	else {
+		cout << "[CREATE DECODER] video - avcodec_open2 success!" << endl;
+	}
+
+	/************************************ 音频解码器部分 ************************************/
+	//找到音频解码器
+	AVCodec* acodec = avcodec_find_decoder(ic->streams[audioStream]->codecpar->codec_id);
+	if (!acodec) {
+		cout << "[CREATE DECODER] Can't find the audio codec id " << ic->streams[audioStream]->codecpar->codec_id << endl;
+		return -3;
+	}
+	else {
+		cout << "[CREATE DECODER] Find the acodec " << ic->streams[audioStream]->codecpar->codec_id << endl;
+	}
+
+	//创建解码器上下文
+	if (ac == NULL) { ac = avcodec_alloc_context3(acodec); }
+
+	//配置解码器上下文参数
+	avcodec_parameters_to_context(ac, ic->streams[audioStream]->codecpar);
+
+	//4线程解码
+	ac->thread_count = 4;
+
+	//打开解码器上下文
+	/*
+		对于avcodec_open2函数的第二个参数，If a non-NULL codec has been previously passed to 
+		avcodec_alloc_context3() or for this context, then this parameter MUST be either NULL 
+		or equal to the previously passed codec.
+	 */
+	re = avcodec_open2(ac, 0, 0);
+	if (0 != re) {		
+		cout << "[CREATE DECODER] avcodec_open2 failed: " << get_error(re) << endl;
+		return -4;
+	}
+	else {
+		cout << "[CREATE DECODER] audio - avcodec_open2 success!" << endl;
+	}
+
+	return 0;
+}
+
 int XFFmpeg::get_duration_ms() {
 	return total_ms;
 }
+
 void XFFmpeg::close() {
 	mutex.lock();
 	if (ic) avformat_close_input(&ic);
+	if (vc)avcodec_free_context(&vc);
+	if (ac)avcodec_free_context(&ac);
+	if (packet) av_packet_free(&packet);
+	if (yuv) av_frame_free(&yuv);
 	mutex.unlock();
 }
+
 std::string XFFmpeg::get_error(int error_num) {	
 	mutex.lock();
 	av_strerror(error_num, error_buf, error_len - 1);
@@ -75,6 +207,7 @@ std::string XFFmpeg::get_error(int error_num) {
 	mutex.unlock();
 	return re;
 }
+
 XFFmpeg::XFFmpeg() {
 	//错误信息缓存初始化
 	error_buf[error_len] = '\0';
