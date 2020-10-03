d6af170905a6dd99e228889f3e6cc1e704cc0a1d
RoundBridge <410308507@qq.com>
2020-10-03 15:16:16 +0800

    support the display of total time of the video

--- XPlay1/xplay1.cpp
+++ XPlay1/xplay1.cpp
@@ -12,6 +12,9 @@ XPlay1::XPlay1(QWidget *parent)
 }
 
 void XPlay1::open() {
+    int totalMs = 0, minutes = 0, seconds = 0;
+    char buf[24] = { 0 };
+
     QString name = QFileDialog::getOpenFileName(this, QString::fromLocal8Bit("打开")); // 英文的话，第二个参数直接就是QString("Open File")
     if (name.isEmpty()) {
         return;
@@ -21,8 +24,17 @@ void XPlay1::open() {
         QMessageBox::information(this, "ERROR: ", "open file failed");
         return;
     }
+
+    totalMs = XFFmpeg::get()->get_duration_ms();
+    minutes = (totalMs / 1000) / 60;
+    seconds = (totalMs / 1000) % 60;
+    sprintf(buf, "00:00/%d:%d", minutes, seconds);
+    ui.playTime->setText(buf);
+
     XVideoThread::isStart = true;
     XVideoThread::isExit = false;
 
     XVideoThread::get()->start();
+
+    return;
 }
